8637e44758e32f06f55089b29bca9ffe1da59746
Jonathan Müller <jo-m@outlook.com>
2022-12-04 21:13:00 +0100

    get rid of last image.Image reference

--- pkg/pmatch/opt.go
+++ pkg/pmatch/opt.go
@@ -22,10 +22,13 @@ func SearchGrayOpt(img, pat *image.Gray) (maxX, maxY int, maxScore float64) {
 	is := img.Stride
 	ps := pat.Stride
 
+	imgX0, imgY0 := img.Rect.Min.X, img.Rect.Min.Y
+	patX0, patY0 := pat.Rect.Min.X, pat.Rect.Min.Y
+
 	for y := 0; y < n; y++ {
 		for x := 0; x < m; x++ {
-			winX0 := img.Rect.Min.X + x
-			winY0 := img.Rect.Min.Y + y
+			winX0 := imgX0 + x
+			winY0 := imgY0 + y
 			imgPatStartIx := y*is + x
 
 			var dot, sqSumI, sqSumP uint64
@@ -35,7 +38,7 @@ func SearchGrayOpt(img, pat *image.Gray) (maxX, maxY int, maxScore float64) {
 					pxIi := ((winY0+v)-winY0)*is + ((winX0+u)-winX0)*1
 					pxI := img.Pix[imgPatStartIx+pxIi]
 
-					pxPi := ((pat.Rect.Min.Y+v)-pat.Rect.Min.Y)*ps + ((pat.Rect.Min.X+u)-pat.Rect.Min.X)*1
+					pxPi := ((patY0+v)-patY0)*ps + ((patX0+u)-patX0)*1
 					pxP := pat.Pix[pxPi]
 
 					dot += uint64(pxI) * uint64(pxP)
