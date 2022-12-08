4a15441ae436586dda140984ddcb07578088b247
Jonathan Müller <jo-m@outlook.com>
2022-12-08 21:40:52 +0100

    add ransac

---
+++ pkg/ransac/ransac.go
@@ -0,0 +1,177 @@
+package ransac
+
+import (
+	"errors"
+	"image/color"
+	"math"
+	"math/rand"
+
+	"github.com/rs/zerolog/log"
+	"go-hep.org/x/hep/fit"
+	"go-hep.org/x/hep/hplot"
+	"gonum.org/v1/gonum/optimize"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+)
+
+func sample(rnd *rand.Rand, x, y []float64, n int) ([]float64, []float64) {
+	if n < 1 {
+		panic("n must be > 0")
+	}
+	if len(x) != len(y) {
+		panic("x and y must have same length")
+	}
+
+	isel := map[int]struct{}{}
+	xret, yret := make([]float64, n), make([]float64, n)
+	for {
+		// random index
+		i := int(rand.Intn(len(x)))
+
+		// make sure is without replacement
+		if _, ok := isel[i]; ok {
+			continue
+		}
+		isel[i] = struct{}{}
+
+		n--
+		xret[n] = x[i]
+		yret[n] = y[i]
+		if n == 0 {
+			return xret, yret
+		}
+	}
+}
+
+type ModelFn func(x float64, ps []float64) float64
+
+type RansacParams struct {
+	MinModelPoints  int
+	MaxIter         int
+	MinInliers      int
+	InlierThreshold float64
+	Seed            int64
+}
+
+func (p *RansacParams) Check(nx int) {
+	if p.MinModelPoints == 0 {
+		panic("MinModelPoints cannot be 0")
+	}
+	if p.MinModelPoints > nx*3/4 {
+		panic("MinModelPoints should be <= len(x)*2/3")
+	}
+	if p.MaxIter == 0 {
+		panic("MaxIter must be > 0")
+	}
+	if p.MinInliers < p.MinModelPoints {
+		panic("MinInliers must be at least MinModelPoints")
+	}
+}
+
+func Ransac(x, y []float64, model ModelFn, p RansacParams) (*optimize.Location, error) {
+	// check inputs
+	if len(x) != len(y) {
+		panic("x and y must have same length")
+	}
+	p.Check(len(x))
+
+	src := rand.NewSource(p.Seed)
+	rnd := rand.New(src)
+
+	var bestFit optimize.Location
+	bestErr := math.MaxFloat64
+
+	// iter
+	// TODO: parallelize?
+	for i := 0; i < p.MaxIter; i++ {
+		// sample
+		xS, yS := sample(rnd, x, y, p.MinModelPoints)
+
+		// fit
+		params, err := fit.Curve1D(
+			fit.Func1D{
+				F:  model,
+				X:  xS,
+				Y:  yS,
+				Ps: []float64{1, 1},
+			},
+			nil, &optimize.NelderMead{},
+		)
+		if err != nil {
+			log.Err(err).Msg("fit did not converge (sample)")
+			continue
+		}
+		// TODO: check status?
+
+		// inliers
+		xIn, yIn := []float64{}, []float64{}
+		for j := range x {
+			// apply model
+			yModel := model(x[j], params.Location.X)
+			// in inlier?
+			if math.Abs(yModel-y[j]) < p.InlierThreshold {
+				xIn = append(xIn, x[j])
+				yIn = append(yIn, y[j])
+			}
+		}
+
+		// fit inliers
+		params, err = fit.Curve1D(
+			fit.Func1D{
+				F:  model,
+				X:  xIn,
+				Y:  yIn,
+				Ps: []float64{1, 1},
+			},
+			nil, &optimize.NelderMead{},
+		)
+		if err != nil {
+			log.Err(err).Msg("fit did not converge (inliers)")
+			continue
+		}
+		// TODO: check status?
+
+		// Plot(
+		// 	fmt.Sprintf("~/Desktop/fit_%03d_%f.png", i, params.Location.F),
+		// 	xIn, yIn, params.Location.X, model, "f(x) = a + b*x*x")
+
+		if params.Location.F < bestErr {
+			bestFit = params.Location
+			bestErr = params.Location.F
+		}
+	}
+
+	if bestErr == math.MaxFloat64 {
+		return nil, errors.New("RANSAC unsuccessful")
+	}
+
+	return &bestFit, nil
+}
+
+func Plot(path string, x, y []float64, ps []float64, fn ModelFn, labelX string) {
+	p := hplot.New()
+	p.X.Label.Text = labelX
+	p.Y.Label.Text = "y-data"
+	p.X.Min = -10
+	p.X.Max = +10
+	p.Y.Min = 0
+	p.Y.Max = 220
+
+	s := hplot.NewS2D(hplot.ZipXY(x, y))
+	s.Color = color.RGBA{0, 0, 255, 255}
+	p.Add(s)
+
+	f := plotter.NewFunction(func(x float64) float64 {
+		return fn(x, ps)
+	})
+	f.Color = color.RGBA{255, 0, 0, 255}
+	f.Samples = 1000
+	p.Add(f)
+
+	p.Add(plotter.NewGrid())
+
+	err := p.Save(20*vg.Centimeter, -1, path)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ pkg/ransac/ransac_test.go
@@ -0,0 +1,61 @@
+package ransac
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_sample(t *testing.T) {
+	x := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	y := []float64{10, 20, 30, 40, 50, 60, 70, 80, 90}
+
+	src := rand.NewSource(123)
+	rnd := rand.New(src)
+
+	for i := 0; i < 1000; i++ {
+		xs, ys := sample(rnd, x, y, 4)
+
+		xset := map[float64]struct{}{}
+		for j := range xs {
+			assert.Equal(t, xs[j]*10, ys[j])
+			xset[xs[j]] = struct{}{}
+		}
+
+		assert.Equal(t, len(xs), len(xset))
+	}
+}
+
+func Test_Ransac(t *testing.T) {
+	testData := []int{
+		34, 34, 34, 34, 34, 26, 0, 34, 1, 1, 0, 0, 20, 0, 34, 34, 34, 34, 25, 34, 34,
+		34, 34, 34, 34, 34, 34, 34, 22, 0, 34, 34, 28, 34, 34, 26, 27, 34, 34, 34, 34,
+		34, 34, 0, 0, 34, 34, 34, 0, 34, 34, 34, 34, 1, 34, 34, 22, 34, 34, 34, 34, 0,
+		34, 0, 34, 34, 26, 34, 34, 34, 3, 34, 34, 32, 34, 34, 34, 7, 0, 34, 0, 34, 1,
+		34, 34, 0, 34, 34, 5, 34, 5, 34, 27, 0, 0, 34, 34, 34, 34, 34, 32, 31, 34, 34,
+		29, 25, 34, 10, 0, 6, 0, 34, 0, 34, 1, 24, 34, 34, 35,
+	}
+
+	poly := func(x float64, ps []float64) float64 {
+		return ps[0] + ps[1]*x*x
+	}
+	xf := make([]float64, len(testData))
+	yf := make([]float64, len(testData))
+	for i := range yf {
+		xf[i] = float64(i)
+		yf[i] = float64(testData[i])
+	}
+
+	fit, err := Ransac(xf, yf, poly, RansacParams{
+		MinModelPoints:  3,
+		MaxIter:         10,
+		MinInliers:      len(xf) / 2,
+		InlierThreshold: 2.,
+		Seed:            123,
+	})
+	require.NoError(t, err)
+	require.InDelta(t, 34, fit.X[0], 0.1)
+	require.InDelta(t, 0, fit.X[1], 0.001)
+}
