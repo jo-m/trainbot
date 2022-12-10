4d701f35eea141e2f33fcbb98ddfe834bbe6ebed
Jonathan Müller <jo-m@outlook.com>
2022-12-10 22:35:01 +0100

    remove unused args

--- cmd/confighelper/main.go
+++ cmd/confighelper/main.go
@@ -5,11 +5,9 @@ import (
 	"image"
 	"io"
 	"net/http"
-	"os"
 
 	"github.com/alexflint/go-arg"
 	"github.com/jo-m/trainbot/internal/pkg/logging"
-	"github.com/jo-m/trainbot/pkg/rec"
 	"github.com/jo-m/trainbot/pkg/server"
 	"github.com/jo-m/trainbot/pkg/vid"
 	"github.com/rs/zerolog/log"
@@ -27,8 +25,6 @@ type config struct {
 	LiveReload bool   `arg:"--live-reload,env:LIVE_RELOAD" default:"false" help:"Live reload WWW static files"`
 	ListenAddr string `arg:"--listen-addr,env:LISTEN_ADDR" default:"localhost:8080" help:"Address and port to listen on"`
 
-	VideoFile string `arg:"--video-file" help:"Video file or directory, e.g. video.mp4 or imgs/20221208_093141.065_+01:00"`
-
 	CameraDevice       string `arg:"--camera-device" help:"Video4linux device file, e.g. /dev/video0"`
 	CameraFormatFourCC string `arg:"--format-fourcc" default:"MJPG" help:"Camera pixel format FourCC string, ignored if using video file"`
 	CameraFrameSizeW   int    `arg:"--framesz-w" default:"1920" help:"Camera frame size width, ignored if using video file"`
@@ -40,38 +36,11 @@ func parseCheckArgs() config {
 	p := arg.MustParse(&c)
 	logging.MustInit(c.LogConfig)
 
-	if c.CameraDevice == "" && c.VideoFile == "" {
-		p.Fail("no camera device and no video file passed")
-	}
-	if c.CameraDevice != "" && c.VideoFile != "" {
-		p.Fail("cannot pass both camera device and video file")
+	if c.CameraDevice == "" {
+		p.Fail("no camera device passed")
 	}
 
 	return c
-}
-
-func openSrc(c config) (vid.Src, error) {
-	if c.CameraDevice != "" {
-		return vid.NewCamSrc(vid.CamConfig{
-			DeviceFile: c.CameraDevice,
-			Format:     vid.FourCC(c.CameraFormatFourCC),
-			FrameSize:  image.Point{c.CameraFrameSizeW, c.CameraFrameSizeH},
-		})
-	}
-
-	var stat os.FileInfo
-	stat, err := os.Stat(c.VideoFile)
-	if err != nil {
-		log.Err(err).Str("path", c.VideoFile).Msg("stat failed")
-		return nil, err
-	}
-	if stat.IsDir() {
-		// image file directory
-		return rec.NewReader(c.VideoFile)
-	} else {
-		// video file
-		return vid.NewFileSrc(c.VideoFile, false)
-	}
 }
 
 func main() {
@@ -84,9 +53,13 @@ func main() {
 		log.Panic().Err(err).Msg("unable to initialize server")
 	}
 
-	src, err := openSrc(c)
+	src, err := vid.NewCamSrc(vid.CamConfig{
+		DeviceFile: c.CameraDevice,
+		Format:     vid.FourCC(c.CameraFormatFourCC),
+		FrameSize:  image.Point{c.CameraFrameSizeW, c.CameraFrameSizeH},
+	})
 	if err != nil {
-		log.Panic().Err(err).Str("path", c.CameraDevice+c.VideoFile).Msg("failed to open video source")
+		log.Panic().Err(err).Str("path", c.CameraDevice).Msg("failed to open video source")
 	}
 
 	go func() {
