7568dfbc045f5b6f4dbd71a810368adcaf121acf
Jonathan Müller <jo-m@outlook.com>
2022-12-10 15:57:23 +0100

    add pmatch test binary

---
+++ examples/pmatch/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"image"
+
+	"github.com/jo-m/trainbot/internal/pkg/imutil"
+	"github.com/jo-m/trainbot/pkg/pmatch"
+)
+
+const (
+	px      = 20
+	py      = 40
+	patchSz = 50
+)
+
+func main() {
+	img := imutil.ToGray(pmatch.LoadTestImg())
+	rect := image.Rect(px, py, patchSz+px, patchSz+py)
+	pat, err := imutil.Sub(img, rect)
+	if err != nil {
+		panic(err)
+	}
+
+	x, y, score := pmatch.SearchGrayC(img, pat.(*image.Gray))
+	fmt.Printf("x=%d y=%d score=%f\n", x, y, score)
+	if x != px {
+		panic("x detected incorrectly")
+	}
+	if y != py {
+		panic("y detected incorrectly")
+	}
+	if score != 1 {
+		panic("invalid score")
+	}
+}
