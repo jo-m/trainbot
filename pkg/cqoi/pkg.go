f7ee4edb7682167858c00d9ea6935bafbb88fe5a
Jonathan Müller <jo-m@outlook.com>
2022-12-21 21:57:19 +0100

    add qoi upstream

---
+++ pkg/cqoi/pkg.go
@@ -0,0 +1,3 @@
+// Package cqoi is a CGo wrapper for the QOI image format from https://github.com/phoboslab/qoi.
+// The current files are from upstream commit c3dcfe780bf28f4a59bca7f0a60e2e18b0acf68f.
+package cqoi
