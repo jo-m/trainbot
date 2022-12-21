93a98d409f250da7f0472e37682c21c391f3bcad
Jonathan Müller <jo-m@outlook.com>
2022-12-21 22:02:14 +0100

    add imutil io test

---
+++ internal/pkg/imutil/io_test.go
@@ -0,0 +1,103 @@
+package imutil
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func getFileSize(t *testing.T, path string) int {
+	stat, err := os.Stat(path)
+	require.NoError(t, err)
+
+	return int(stat.Size())
+}
+
+func Test_Dump_Size_PNG(t *testing.T) {
+	dir := t.TempDir()
+	fname := path.Join(dir, "out.png")
+	img := RandRGBA(123, 200, 500)
+
+	err := Dump(fname, img)
+	assert.NoError(t, err)
+	assert.Equal(t, 400772, getFileSize(t, fname))
+}
+func Test_Dump_Size_JPG(t *testing.T) {
+	dir := t.TempDir()
+	fname := path.Join(dir, "out.jpg")
+	img := RandRGBA(123, 200, 500)
+
+	err := Dump(fname, img)
+	assert.NoError(t, err)
+	err = Dump(fname, img)
+	assert.NoError(t, err)
+	assert.Equal(t, 160675, getFileSize(t, fname))
+}
+
+func Test_Dump_Size_QOI(t *testing.T) {
+	dir := t.TempDir()
+	fname := path.Join(dir, "out.qoi")
+	img := RandRGBA(123, 200, 500)
+
+	err := Dump(fname, img)
+	assert.NoError(t, err)
+	assert.Equal(t, 499632, getFileSize(t, fname))
+}
+
+func Benchmark_Dump_PNG(b *testing.B) {
+	dir := b.TempDir()
+	img := RandRGBA(123, 200, 500)
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		fname := path.Join(dir, fmt.Sprintf("out_%05d.png", i))
+
+		b.StartTimer()
+		err := Dump(fname, img)
+		b.StopTimer()
+
+		if err != nil {
+			b.Error(err)
+		}
+	}
+}
+
+func Benchmark_Dump_JPG(b *testing.B) {
+	dir := b.TempDir()
+	img := RandRGBA(123, 200, 500)
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		fname := path.Join(dir, fmt.Sprintf("out_%05d.jpg", i))
+
+		b.StartTimer()
+		err := Dump(fname, img)
+		b.StopTimer()
+
+		if err != nil {
+			b.Error(err)
+		}
+	}
+}
+
+func Benchmark_Dump_QOI(b *testing.B) {
+	dir := b.TempDir()
+	img := RandRGBA(123, 200, 500)
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		fname := path.Join(dir, fmt.Sprintf("out_%05d.qoi", i))
+
+		b.StartTimer()
+		err := Dump(fname, img)
+		b.StopTimer()
+
+		if err != nil {
+			b.Error(err)
+		}
+	}
+}
