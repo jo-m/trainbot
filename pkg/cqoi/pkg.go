0417d03adf8192ff736c638f6cec9522918f7c51
Jonathan Müller <jo-m@outlook.com>
2023-02-19 19:23:30 +0100

    update upstream qoi

--- pkg/cqoi/pkg.go
+++ pkg/cqoi/pkg.go
@@ -1,3 +1,3 @@
 // Package cqoi is a CGo wrapper for the QOI image format from https://github.com/phoboslab/qoi.
-// The current files are from upstream commit c3dcfe780bf28f4a59bca7f0a60e2e18b0acf68f.
+// The current files are from upstream commit 0d8d07971b12b9ab3bf4fb721676a11c1a0252ac.
 package cqoi
