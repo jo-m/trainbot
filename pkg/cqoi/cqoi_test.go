1d0e2c98ed29d8d117dddfd0946322b3fca5a7ed
Jonathan Müller <jo-m@outlook.com>
2023-02-19 19:23:09 +0100

    make private

--- pkg/cqoi/cqoi_test.go
+++ pkg/cqoi/cqoi_test.go
@@ -2,14 +2,33 @@ package cqoi
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"math/rand"
 	"path"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func randRGBA(seed int64, w, h int) *image.RGBA {
+	src := rand.NewSource(seed)
+	rnd := rand.New(src)
+
+	rect := image.Rect(0, 0, w, h)
+	img := image.NewRGBA(rect)
+
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			px := color.RGBA{uint8(rnd.Int()), uint8(rnd.Int()), uint8(rnd.Int()), uint8(rnd.Int())}
+			img.Set(x, y, px)
+		}
+	}
+
+	return img
+}
 func Test_Dump_Load(t *testing.T) {
-	img := RandRGBA(123, 200, 500)
+	img := randRGBA(123, 200, 500)
 	dir := t.TempDir()
 	fname := path.Join(dir, "img.qoi")
 
@@ -24,7 +43,7 @@ func Test_Dump_Load(t *testing.T) {
 
 func Benchmark_Dump_QOI(b *testing.B) {
 	dir := b.TempDir()
-	img := RandRGBA(123, 200, 500)
+	img := randRGBA(123, 200, 500)
 
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
