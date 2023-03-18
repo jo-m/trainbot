31861a723d0bbb98e5c635b97d1f2e5d42cdb29e
Jonathan Müller <jo-m@outlook.com>
2023-03-19 00:18:37 +0100

    yuv420

---
+++ pkg/vid/yuv420.go
@@ -0,0 +1,16 @@
+package vid
+
+import (
+	"image"
+)
+
+// NewYuv420 creates a new image.Image from a raw Yuv420p buffer.
+// The returned image will reference the passed buffer and no data is copied.
+func NewYuv420(buf []byte, w, h int) image.Image {
+	im := image.NewYCbCr(image.Rect(0, 0, w, h), image.YCbCrSubsampleRatio420)
+	im.Y = buf
+	im.Cb = buf[len(buf)/12*8:]
+	im.Cr = buf[len(buf)/12*10:]
+
+	return im
+}
---
+++ pkg/vid/yuv420_test.go
@@ -0,0 +1,26 @@
+package vid
+
+import (
+	"os"
+	"testing"
+
+	"github.com/jo-m/trainbot/internal/pkg/imutil"
+	"github.com/jo-m/trainbot/internal/pkg/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	w = 512
+	h = 256
+)
+
+func Test_NewYuv420(t *testing.T) {
+	buf, err := os.ReadFile("testdata/512x256.yuv420p.data")
+	require.NoError(t, err)
+	require.Equal(t, len(buf), w*h*12/8)
+
+	im := NewYuv420(buf, w, h)
+	truth, err := imutil.Load("testdata/512x256.yuv420p.jpg")
+	require.NoError(t, err)
+	testutil.AssertImagesAlmostEqual(t, truth, im)
+}
