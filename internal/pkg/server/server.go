bf9ef3b0d592cc60419c448fd5b9da4ce0a642d1
Jonathan Müller <jo-m@outlook.com>
2023-03-19 14:27:58 +0100

    refactor server

--- internal/pkg/server/server.go
+++ internal/pkg/server/server.go
@@ -2,18 +2,25 @@ package server
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"image"
 	"image/jpeg"
 	"net/http"
 
+	"github.com/jo-m/trainbot/pkg/vid"
 	"github.com/mattn/go-mjpeg"
+	"github.com/rs/zerolog/log"
 )
 
+// Server is a simple MJPEG HTTP streaming server.
+// Use NewServer to initiate an instance.
 type Server struct {
 	mux    *http.ServeMux
 	stream *mjpeg.Stream
 }
 
+// NewServer creates a new server.
 func NewServer(embed bool) (*Server, error) {
 	mux := http.NewServeMux()
 	s := Server{
@@ -35,10 +42,34 @@ func NewServer(embed bool) (*Server, error) {
 	return &s, nil
 }
 
+// handleCameras detects v4l cameras and returns them as JSON.
+// Test via
+//
+//	http localhost:8080/cameras
+func (s *Server) handleCameras(resp http.ResponseWriter, req *http.Request) {
+	cams, err := vid.DetectCams()
+	if err != nil {
+		log.Err(err).Send()
+
+		resp.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(resp, err.Error())
+		return
+	}
+
+	resp.Header().Add("content-type", "application/json")
+	resp.WriteHeader(http.StatusOK)
+	err = json.NewEncoder(resp).Encode(cams)
+	if err != nil {
+		log.Err(err).Send()
+	}
+}
+
+// GetMux returns the router.
 func (s *Server) GetMux() *http.ServeMux {
 	return s.mux
 }
 
+// SetFrame encodes an image as JPEG and streams it.
 func (s *Server) SetFrame(frame image.Image) error {
 	buf := bytes.Buffer{}
 	err := jpeg.Encode(&buf, frame, nil)
@@ -48,6 +79,8 @@ func (s *Server) SetFrame(frame image.Image) error {
 	return s.stream.Update(buf.Bytes())
 }
 
+// SetFrameRawJPEG streams a raw encoded JPEG frame.
 func (s *Server) SetFrameRawJPEG(frame []byte) error {
-	return s.stream.Update(frame)
+	cp := append([]byte(nil), frame...)
+	return s.stream.Update(cp)
 }
