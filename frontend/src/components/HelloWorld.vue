8af6433a52e5f7a84b50ab9b48de58958171713e
Jonathan Müller <jo-m@outlook.com>
2023-03-29 07:43:51 +0200

    POC: use SQL db from VueJS app

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -10,6 +10,26 @@ defineProps<{}>()
   </div>
 </template>
 
+<script lang="ts">
+import initSqlJs from "sql.js";
+import sqlWasmUrl from "sql.js/dist/sql-wasm.wasm?url";
+
+console.log(sqlWasmUrl)
+const sqljs = await initSqlJs({locateFile: () => sqlWasmUrl});
+console.log(sqljs)
+const dbFile = await fetch("db.sqlite3");
+console.log(dbFile);
+const dbBuf = await dbFile.arrayBuffer()
+console.log(dbBuf)
+const db = new sqljs.Database(new Uint8Array(dbBuf));
+console.log(db)
+
+const result = db.exec("SELECT * FROM trains ORDER BY start_ts DESC LIMIT 1;")
+console.log(result)
+
+</script>
+
+
 <style scoped>
 h1 {
   font-weight: 500;
