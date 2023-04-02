04fc6f1d4318a80f26af4ce7e527c80b6b857ef9
Jonathan Müller <jo-m@outlook.com>
2023-04-02 22:04:15 +0200

    make routing work on web server

--- frontend/src/plugins/router.ts
+++ frontend/src/plugins/router.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import TrainsListView from '@/views/TrainsListView.vue'
 import TrainDetailView from '@/views/TrainDetailView.vue'
 import TrainsDBProvider from '@/views/TrainsDBProvider.vue'
 import NotFound from '@/views/NotFound.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
