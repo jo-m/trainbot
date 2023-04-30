3253a8ef9dea16cb14fbce3b2e65010c5d7f5967
Jonathan Müller <jo-m@outlook.com>
2023-05-01 00:28:15 +0200

    fix AssertImagesAlmostEqual() to work with arbitrary bounds

--- internal/pkg/testutil/image.go
+++ internal/pkg/testutil/image.go
@@ -17,7 +17,7 @@ func iabs(i int) int {
 
 // AssertImagesAlmostEqual asserts that two images are almost equal.
 func AssertImagesAlmostEqual(t *testing.T, truth image.Image, test image.Image) {
-	assert.Equal(t, truth.Bounds().Size(), test.Bounds().Size())
+	assert.Equal(t, truth.Bounds().Size(), test.Bounds().Size(), "image size not equal")
 	if truth.Bounds().Size() != test.Bounds().Size() {
 		return
 	}
@@ -25,8 +25,8 @@ func AssertImagesAlmostEqual(t *testing.T, truth image.Image, test image.Image) 
 	var diff uint64
 	for y := 0; y < truth.Bounds().Dy(); y++ {
 		for x := 0; x < truth.Bounds().Dx(); x++ {
-			r0, g0, b0, _ := truth.At(x, y).RGBA()
-			r1, g1, b1, _ := test.At(x, y).RGBA()
+			r0, g0, b0, _ := truth.At(x+truth.Bounds().Min.X, y+truth.Bounds().Min.Y).RGBA()
+			r1, g1, b1, _ := test.At(x+test.Bounds().Min.X, y+test.Bounds().Min.Y).RGBA()
 
 			diff += uint64(iabs(int(r0)-int(r1)) / 255)
 			diff += uint64(iabs(int(g0)-int(g1)) / 255)
@@ -35,5 +35,5 @@ func AssertImagesAlmostEqual(t *testing.T, truth image.Image, test image.Image) 
 	}
 
 	diffPerPx := float64(diff) / float64(truth.Bounds().Dx()) / float64(truth.Bounds().Dy()) / 3
-	assert.Less(t, diffPerPx, 1.)
+	assert.Less(t, diffPerPx, 1., "difference per pixel is higher than allowed")
 }
