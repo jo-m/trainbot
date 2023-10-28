64d5b2da453bd75e944dc5be859aad2fdcff4400
Jonathan Müller <jo-m@outlook.com>
2023-10-28 14:13:25 +0200

    formatting

--- frontend/src/lib/stats.ts
+++ frontend/src/lib/stats.ts
@@ -5,20 +5,19 @@ export const avgLengthM = (db: SqlJs.Database): number =>
   queryOne(
     db,
     `SELECT
-        SUM(ABS(length_px / px_per_m)) / COUNT(*)
+      SUM(ABS(length_px / px_per_m)) / COUNT(*)
     FROM trains;`
   ) as number
 
 export const histCountPerDayOfWeek = (db: SqlJs.Database): number[][] =>
-  db.exec(
-    `SELECT
-        -- have to shift by 1, because SQLite thinks 0 = Sunday
-        (CAST(strftime('%w', start_ts) AS INT) + 6) % 7 AS dow,
-        COUNT(*)
+  db.exec(`
+    SELECT
+      -- have to shift by 1, because SQLite thinks 0 = Sunday
+      (CAST(strftime('%w', start_ts) AS INT) + 6) % 7 AS dow,
+      COUNT(*)
     FROM trains
     GROUP BY dow
-    ORDER BY dow;`
-  )[0].values as number[][]
+    ORDER BY dow;`)[0].values as number[][]
 
 export const dayOfWeekLabels: { [key: number]: string } = {
   0: 'Mon',
@@ -31,38 +30,35 @@ export const dayOfWeekLabels: { [key: number]: string } = {
 }
 
 export const histHourOfDay = (db: SqlJs.Database): number[][] =>
-  db.exec(
-    `SELECT
-      CAST(strftime('%H', start_ts) AS INT) AS hod,
-      COUNT(*)
+  db.exec(`
+  SELECT
+    CAST(strftime('%H', start_ts) AS INT) AS hod,
+    COUNT(*)
   FROM trains
   GROUP BY hod
-  ORDER BY hod;`
-  )[0].values as number[][]
+  ORDER BY hod;`)[0].values as number[][]
 
 // no magic needed to fill in missing bins, we have values for everything
 export const histSpeedKPH = (db: SqlJs.Database, binSz: number = 10): number[][] =>
-  db.exec(
-    `WITH speed_rounded AS (
-      SELECT
-        CAST(ABS(speed_px_s / px_per_m * 3.6)/${binSz} AS INTEGER) * ${binSz} AS speed_rounded
-      FROM trains
-    )
+  db.exec(`
+  WITH speed_rounded AS (
     SELECT
-      speed_rounded,
-      COUNT(*)
+      CAST(ABS(speed_px_s / px_per_m * 3.6)/${binSz} AS INTEGER) * ${binSz} AS speed_rounded
+    FROM trains
+  )
+  SELECT
+    speed_rounded,
+    COUNT(*)
   FROM speed_rounded
   GROUP BY speed_rounded
-  ORDER BY speed_rounded;`
-  )[0].values as number[][]
+  ORDER BY speed_rounded;`)[0].values as number[][]
 
 export const tempDegCPast24hAvg = (db: SqlJs.Database): number[][] =>
-  db.exec(
-    `SELECT
+  db.exec(`
+  SELECT
     CAST(strftime('%H', timestamp) AS INT) AS hod,
     ROUND(AVG(temp_deg_c))
   FROM temperatures
   WHERE timestamp >= DATETIME('now','-24 hours')
   GROUP BY hod
-  ORDER BY hod ASC`
-  )[0].values as number[][]
+  ORDER BY hod ASC`)[0].values as number[][]
