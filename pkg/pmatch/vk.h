d49aff981f4635d93048f52b551e1c86287ceb6d
Jonathan Müller <jo-m@outlook.com>
2023-12-10 11:19:38 +0100

    add "real" error handling

--- pkg/pmatch/vk.h
+++ pkg/pmatch/vk.h
@@ -3,8 +3,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
+// TODO: compiler flags
 // TODO: split generic vk functionality into separate files.
-// TODO: proper error handling, get rid of asserts
 
 // Buffers: uint8_t *
 // Sizes: size_t
@@ -23,9 +23,15 @@ typedef struct dim3 {
   uint32_t z;
 } dim3;
 
-void prepare(size_t img_sz, size_t pat_sz, size_t search_sz,
-             const uint8_t *shader, size_t shader_sz, int32_t *spec_consts,
-             uint32_t spec_consts_count);
-void run(results *out, const uint8_t *img, const uint8_t *pat, uint8_t *search,
-         const dim3 wg_sz);
+// TODO: replace with VkResult.
+typedef int vk_result;
+#define vk_success (0)
+
+vk_result prepare(size_t img_sz, size_t pat_sz, size_t search_sz,
+                  const uint8_t *shader, size_t shader_sz,
+                  int32_t *spec_consts, uint32_t spec_consts_count)
+    __attribute((warn_unused_result));
+vk_result run(results *out, const uint8_t *img, const uint8_t *pat,
+              uint8_t *search, const dim3 wg_sz)
+    __attribute((warn_unused_result));
 void cleanup();
